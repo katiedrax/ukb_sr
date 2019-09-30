4b7c1b23564d32807529c04b70b30ddb66771e3d
Katie Drax <zn18986@bristol.ac.uk>
2019-10-01 00:49:53 +0100

    * update todo * function to check dois * remove duplicate rows from source * check & correct dois of source, cite & scopus * don't fill from left in separate * check & correct dois in cite * code creating scopus search string copied into 00_clean_csv * move details about data creation into READMEs * function that will identify all duplicates of one column * remove messy code checking dois have same title * create cleaned title substring * make all doi vectors lowercase * change name of all merged dfs to 'merged'

--- rscripts/01_merge_refs_cits_scopus.R
+++ rscripts/01_merge_refs_cits_scopus.R
@@ -3,14 +3,33 @@
 # This code merges the included refereneces (from Endnote), their citations (from Endnote) and their scopus profiles (exported from scopus as csv)
 # It only cleans the datasets in so far as to allow them to merge correctly and assign headers and export merged csv
 
-
-## TO DO merge on title (after removing all punct etc) or article id
+#TO DO check merges, change incorrect titles & remerge
 
 #########
 #### packages####
 #########
 
 library(tidyverse)
+
+
+###################
+#### functions ####
+###################
+
+# create function to check no missing, duplicated or incorrect dois
+
+check_doi <- function(doi_col){
+  # check no dois missing
+  if(sum(is.na(doi_col)) > 0) stop("dois missing") else
+    # check all dois contain 10.
+    if(length(doi_col[!grepl("10", doi_col)]) != 0) stop("some dois don't contain 10") else
+      # check all dois start with 10
+      if(length(doi_col[!grepl("^10", doi_col)]) != 0) stop("some dois don't start with 10") else
+        # check no dois duplicated
+        if(sum(duplicated(doi_col)) > 0) stop("duplicate dois") else
+          # correct
+          return("dois OK")
+}
 
 ###################
 #### import source ####
@@ -24,34 +43,25 @@ source <- read.csv("data/02_source_w_headers.csv", header = T, stringsAsFactors 
 #### clean source ####
 #################
 
-# function to remove any duplicates
+# remove duplicate rows
 
-remove_dups <- function(df, col1, col2){
-  cols_to_check <- c(col1, col2)
-  df <- df[!duplicated(df[, cols_to_check]), ]
-}
+source <- source[!duplicated(source[, c("title", "doi")]), ]
 
-# check & remove duplicates
+# check dois that aren't missing
 
-if(sum(duplicated(source[, c("title", "doi")])) > 0){
-  #remove dups
-  source <- remove_dups(source, "title", "doi")
-} else {
-  print("no dups")
-}
+check_doi(na.omit(source$doi))
 
-# function to check each row has a unique doi (not counting NA as a value)
+# check if any dois don't start with 10 
 
-check_doi <- function(doi_col, df){
-  if((length(na.omit(unique(doi_col))) == nrow(df)) == FALSE)
-    stop("missing/duplicated dois")
-  else
-    print("dois OK")
-}
+source$doi[!grepl("^10", source$doi)]
 
-# check dois
+# remove everything that comes before 10. in dois that don't start with 10
 
-check_doi(source$doi, source)
+source$doi[!grepl("^10", source$doi)] <- sub(".*(?=10\\.)","",source$doi[!grepl("^10", source$doi)], perl=TRUE)
+
+# check dois that aren't missing again
+
+check_doi(na.omit(source$doi))
 
 # check each row has unique doi or is missing
 
@@ -69,13 +79,17 @@ cite <- read.csv("data/03_citations.csv", header = F, encoding = "UTF-8")
 
 colnames(cite)[1] <- "citation"
 
+###################
+#### clean citations####
+###################
+
 #  check number of rows
 
 nrow(cite)
 
 # create doi col by separating it from end of citations
 
-cite <- separate(cite, citation, into = c("temp", "doi"), sep = "doi:", fill = "left", remove = FALSE)
+cite <- separate(cite, citation, into = c("temp", "doi"), sep = "doi:", remove = FALSE)
 
 # remove temp col
 
@@ -83,227 +97,214 @@ cite$temp <- NULL
 
 # remove duplicates
 
-if(sum(duplicated(cite[, "doi"])) > 0){
+if(sum(duplicated(cite)) > 0){
   #remove dups
-  cite <- remove_dups(cite, "doi", col2 = NULL)
+  cite <- cite[!duplicated(cite), ]
+} else {
+  print("no dups")
+}
+
+# check dois that aren't missing
+
+check_doi(na.omit(cite$doi))
+
+# check if any dois don't start with 10 
+
+cite$doi[!grepl("^10", cite$doi)]
+
+# remove everything that comes before 10. in dois that don't start with 10
+
+cite$doi[!grepl("^10", cite$doi)] <- sub(".*(?=10\\.)","",cite$doi[!grepl("^10", cite$doi)], perl=TRUE)
+
+# check dois that aren't missing again
+
+check_doi(na.omit(cite$doi))
+
+###############
+#### import included ####
+###############
+
+# import references from cleaned csv file
+
+df <- read.csv("outputs/csv_clean.csv", header =T, stringsAsFactors = F, encoding = "UTF-8")
+
+
+##################
+#### import scopus results ####
+#################
+
+# import csv exported from scopus
+
+scopus <- read.csv("data/scopus.csv", header = T, stringsAsFactors = F, encoding = "UTF-8")
+
+##################
+#### clean scopus ####
+#################
+
+# drop affiliations col, this had to be exported to export the 'authors with affiliations' col
+
+scopus$Affiliations <- NULL
+
+# check & remove duplicates
+
+if(sum(duplicated(scopus)) > 0){
+  #remove dups
+  scopus <- scopus[!duplicated(scopus), ]
 } else {
   print("no dups")
 }
 
 # check dois
 
-check_doi(cite$doi, cite)
+check_doi(scopus$DOI)
 
-###############
-#### import included ####
-###############
+# function to find all duplicate values in one vector
 
-#import references from cleaned csv file
-
-df <- read.csv("outputs/csv_clean.csv", header =T, stringsAsFactors = F, encoding = "UTF-8")
-
-# remove all unicode characters from df
-
-df <- data.frame(lapply(df, function(x) {
-  gsub("[^\u0001-\u007F]+", "", x)
-}))
-
-#################################
-#### create scopus search string ####
-##################################
-
-# create search string (to be pasted into Scopus)
-
-doi_search <- paste("DOI(", df$doi, ")", sep = "", collapse = " OR ")
-
-# export the search string
-
-write.table(doi_search, "outputs/scopus_search_string.txt", quote = F, row.names = F, col.names = F)
-
-##################
-#### import scopus results ####
-#################
-
-# NOT AUTOMATED: paste scopus_search_string into advanced search in Scopus & click search
-# select 'All' > click drop down arrow next to 'RIS export' > select 'CSV' > select all items you want to export > 
-# click 'Export' > save csv as 'scopus' in this project's 'data' folder
-
-# import csv exported from scopus
-
-scopus <- read.csv("data/scopus.csv", header = T, stringsAsFactors = F, encoding = "UTF-8")
-
-# drop affiliations col, this had to be exported to export the 'authors with affiliations' col
-
-scopus$Affiliations <- NULL
-
-# find duplicate DOIs
-
-dup_doi <- scopus$DOI[duplicated(scopus$DOI)]
-
-id <- which(scopus$DOI %in% dup_doi)
-
-if ((length(id) > 0)) {
-  stop(dups <- scopus[id, ])
-} else {
-  print("no dups")
+match_dups <- function(df, col){
+  dups <- col[duplicated(col)]
+  id <- which(col %in% dups)
+  dups <- df[id, ]
 }
 
-# NOT AUTOMATED - I manually checked dois in dups are correct = 1 is not correct
-# replace dois of duplicate that has an incorrect doi 
+# find all duplicate dois
+
+dup_dois <- match_dups(scopus, scopus$DOI)
+
+# NOT AUTOMATED - google dois in dup_dois 
+# replace incorrect dois
 
 scopus$DOI[which(scopus$Title == "A case of recent myocardial infarction with cardiac failure")] <- "10.1136/heartjnl-2016-309715"
 
-#check duplicates again
+#check dois again
 
-dup_doi <- scopus$DOI[duplicated(scopus$DOI)]
+check_doi(scopus$DOI)
 
-id <- which(scopus$DOI %in% dup_doi)
+###################### 
+#### prepare for merge ####
+######################
 
-if ((length(id) > 0)) {
-  stop(dups <- scopus[id, ])
-} else {
-  print("no dups")
+# function to clean a string vector and extract substring
+
+clean_str <- function(string){
+  #remove punctionation
+  string  <- gsub("[[:punct:]]", "", string)
+  #remove punctionation
+  string  <- gsub("–", "", string)
+  #remove punctionation
+  string <- gsub("[^\u0001-\u007F]+", "", string)
+  # remove any unicode characters
+  string <- gsub("[^\u0001-\u007F]+","", string)
+  # remove any numbers 
+  string <- gsub("[0-9]", "", string)
+  #make all strings lowercase
+  string <- tolower(string)
+  #remove all spaces
+  string <- gsub(" ", "", string)
+  # extract substring
+  string <- substring(string, 1,50)
 }
 
-##################################
-#### check same dois have same title ####
-##################################
+# add clean title string to merge on to datasets with a title col (df, scopus & source)
 
-# get df of all titles & dois included refs
-in_df <- select(df, c(title, doi)) %>%
-  # rename doi to be same format as scopus DOI col
-  rename(DOI = "doi") %>%
-  #add col to indicate where data contained in
-  mutate(source = rep("refs"))
+df$ti_clean <- clean_str(df$title)
+scopus$ti_clean <- clean_str(scopus$Title)
+source$ti_clean <- clean_str(source$title)
 
-# get df of all titles & dois included refs in scopus results 
+# dois are case insensitive so make all dois strings lowercase
 
-in_scopus <- select(scopus, c(Title, DOI)) %>%
-  #rename title to be same format as df title col
-  rename(title = "Title") %>%
-  #add col to indicate where data contained in
-  mutate(source = rep("scopus"))
-
-# convert all to character
-
-in_scopus[] <- lapply(in_scopus, as.character)
-in_df[] <- lapply(in_df, as.character)
-
-# make all lower 
-
-in_scopus[] <- lapply(in_scopus, tolower)
-in_df[] <- lapply(in_df, tolower)
-
-# merge everything together
-all <- merge(in_df, in_scopus, all= T)
-
-# remove any punctuation  from title to make sure identical titles with different punctuation are matched
-all$title  <- gsub("[[:punct:]]", "", all$title)
-all$title  <- gsub("–", "", all$title)
-
-#many titles are identicial but have some different characters so >
-# add col of title substrings to make it easier to find duplicate titles then >
-# find items with duplicated title substring and doi ( this means refs have same dois & titles in scopus search)
-
-all$sub <- substring(all$title, 1,35)
-
-dup_sub_doi <- all[duplicated(all[, c("DOI", "sub")]), ]
-
-matches <- all[all$DOI %in% dup_sub_doi$DOI, ]
-
-# find refs that were not in matches & check
-
-not_matches <- all[!(all$DOI %in% dup_sub_doi$DOI), ]
-
-id <- not_matches[duplicated(not_matches[, "DOI"]), ]
-dup_doi <- not_matches[not_matches$DOI %in% id$DOI, ]
-
-print("all not_matches with same DOI have same title just have slight differences in spacing etc")
-
-not_dup_doi <- not_matches[!(not_matches$DOI %in% id$DOI), ]
-
-print("not_dup_doi shows refs that do not have a matching scopus result BUT also shows one scopus result that is not an included reference")
-
-#################
-#### add citations ####
-#################
-
-# convert all cols to characters
-df[] <- lapply(df, as.character)
-cite[] <- lapply(cite, as.character)
-
-# check colnames
-
-colnames(df)
-colnames(cite)
-
-# merge with citation
-
-df_cite <- full_join(df, cite, by = "doi")
-
-# check correct # cols
-
-length(colnames(df)) +length(colnames(cite)) == length(colnames(df_cite)) + 1
-
-# check title still contains 564 included refs
-
-length(df_cite$title[!is.na(df_cite$title)]) == 564
+df$doi <- tolower(df$doi)
+scopus$DOI <- tolower(scopus$DOI)
+cite$doi <- tolower(cite$doi)
+source$doi <- tolower(source$doi)
 
 ##############
 #### add scopus ####
 ##############
 
 # convert all cols to characters
-df_cite[] <- lapply(df_cite, as.character)
+df[] <- lapply(df, as.character)
 scopus[] <- lapply(scopus, as.character)
 
 # check colnames
 
-colnames(df_cite)
+colnames(df)
 colnames(scopus)
 
 # merge with citation
 
-df_cite_scop <- full_join(df_cite, scopus, by = c("doi" = "DOI"))
+merged <- full_join(df, scopus, by = c("doi" = "DOI", "ti_clean"))
 
 # check correct # cols
 
-length(colnames(df_cite)) +length(colnames(scopus)) == length(colnames(df_cite_scop)) + 1
+length(colnames(df)) +length(colnames(scopus)) == length(colnames(merged)) + 2
 
 # check title still contains 564 included refs
 
-length(df_cite_scop$title[!is.na(df_cite_scop$title)]) == 564
+length(na.omit(merged$title)) == 564
 
+# TO DO CHECK MERGES
+#merged$title[is.na(merged$Title)]
+
+#dup_dois <- match_dups(merged, merged$doi)
+
+#dup_titles <- match_dups(merged, merged$ti_clean)
+
+#setdiff(dup_titles$title, dup_titles$Title)
+
+#################
+#### add citations ####
+#################
+
+# convert all cols to characters
+merged[] <- lapply(merged, as.character)
+cite[] <- lapply(cite, as.character)
+
+# check colnames
+
+colnames(merged)
+colnames(cite)
+
+# merge with citation
+
+merged <- full_join(merged, cite, by = "doi")
+
+# check correct # cols
+
+length(colnames(df)) +length(colnames(cite)) == length(colnames(merged)) + 1
+
+# check title still contains 564 included refs
+
+length(na.omit(merged$title)) == 564
 
 #################
 #### add source ####
 ##################
 
 # convert all cols to characters
-df_cite_scop[] <- lapply(df_cite_scop, as.character)
+merged[] <- lapply(merged, as.character)
 source[] <- lapply(source, as.character)
 
 # check colnames
 
-colnames(df_cite_scop)
+colnames(merged)
 colnames(source)
 
 # add source to df
 
-df_cite_scop_sourc <- full_join(df_cite_scop, source, by = "doi")
+merged <- full_join(merged, source, by = c("doi", "ti_clean"))
 
 # check correct num of cols
 # num of cols of two merged dfs combined should == num of cols in new df + 1 (the col you merged on)
 
-length(colnames(df_cite_scop)) +length(colnames(source)) == length(colnames(df_cite_scop_sourc)) + 1
+length(colnames(df)) +length(colnames(source)) == length(colnames(merged)) + 1
 
 # check title still contains 564 included refs
 
-length(df_cite_scop_sourc$title[!is.na(df_cite_scop_sourc$title)]) == 564
+length(na.omit(merged)) == 564
 
 
 #########
 #### export ####
 #########
 
-write.csv(df_cite_scop_sourc, "outputs/merged.csv", row.names = F, fileEncoding = "UTF-8")
+write.csv(merged, "outputs/merged.csv", row.names = F, fileEncoding = "UTF-8")
