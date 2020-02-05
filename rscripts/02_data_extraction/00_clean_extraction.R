9d8f055189b267356fa6f863586a40c4cbb31628
katiedrax <katie.drax@bristol.ac.uk>
2020-02-05 16:57:55 +0000

    update input to most recent, don't remove text as this can be added later, select only unique matches so don't get duplicate matches, extract matches

--- rscripts/02_data_extraction/00_clean_extraction.R
+++ rscripts/02_data_extraction/00_clean_extraction.R
@@ -11,13 +11,31 @@ library(stringr)
 
 source("rscripts/functions/clean-string-fun.R")
 
+
+# find matches
+extract_match <- function(patterns, string){
+  if(is.null(patterns) | !is.character(patterns)) stop("pattern empty or not character")
+  if(is.null(string) | !is.character(string)) stop("string empty or not character")
+  a <- data.frame(match = character(0), string = character(0))
+  for (i in match){
+    out <- data.frame(pattern = i, string = grep(i, string, value = T))
+    a <- rbind(a, out)
+  }
+  if(nrow(a) == length(match)) {
+    return(a)
+  } else {
+    warning("more than one match per value in string")
+    return(a)
+  }
+}
+
 ##########
 # import####
 ##########
 
 # assign input file 
 
-input <- "data/data_extraction/Data+Extraction+Form_4+February+2020_20.22.csv"
+input <- "data/data_extraction/Data+Extraction+Form_5+February+2020_16.24.csv"
 
 # Import first two rows of the  Qualtrics csv export with first row as headers
 
@@ -77,36 +95,37 @@ colnames(dict)[colnames(dict) == "1"] <- "question"
 
 # extract variables from questions that start "[variable]."
 
-var.match <- grep("^\\w+\\.", dict$question, value = T) %>%
-  # remove the columns containing text entries (qualtrics exports these as the question plus "- Text" at the end)
-  .[-grep("\\- Text", .)]
+var.match <- grep("^\\w+\\.", dict$question, value = T)
 
-# find matches
-extract_match <- function(patterns, string){
-  if(is.null(patterns) | !is.character(patterns)) stop("pattern empty or not character")
-  if(is.null(string) | !is.character(string)) stop("string empty or not character")
-  a <- data.frame(match = character(0), string = character(0))
-  for (i in match){
-    out <- data.frame(pattern = i, string = grep(i, string, value = T))
-    a <- rbind(a, out)
-  }
-  if(nrow(a) == length(match)) {
-    return(a)
-  } else {
-    warning("more than one match per value in string")
-    return(a)
-  }
-}
-
-# extract matches for variable names at the start of a string in the format "[variable]."
+# extract matches for var.match
 m <- regexpr("^\\w+\\.",var.match)
 match <- regmatches(var.match, m)%>%
   #escape dots so will be searched for in for-loop
-  gsub("\\.", "\\\\.", .)
+  gsub("\\.", "\\\\.", .) %>%
+  unique(.)
+
+vars <- extract_match(match, var.match)
+
 
 # extract matches for strobe variable names which are a 'matrix' questions >
 # their format is "[matrix question text] - [strobe item number]."
-var.extract <- extract_match(match, var.match)
 
-m <- regexpr("- [1-9]{1,}.",dict$question)
-match <- regmatches(dict$question, m)
+matrixmatch <- grep("\\- [1-9]{1,}.*\\.", dict$question, value = T)
+
+m <- regexpr("\\- [1-9]{1,}.*\\.",matrixmatch)
+match <- regmatches(matrixmatch, m) %>%
+  #escape dots so will be searched for in for-loop %>%
+  gsub("\\.", "\\\\.", .) %>%
+  unique(.)
+
+matrix_vars <- extract_match(match, matrixmatch)
+
+dict2 <- rbind(matrix_vars, vars)
+colnames(vars)[colnames(vars) == "string"] <- "question"
+colnames(vars)[colnames(vars) == "pattern"] <- "variable"
+
+# add data dict of qualtrics cols
+
+qual_dict <- data.frame(question = as.character(header[1, 1:10]), variable = colnames(header)[1:10])
+
+dict2 <- rbind(vars, qual_dict)
