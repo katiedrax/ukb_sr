ecd11f70dfc4ae27942f527a70d607965ecbe2a7
Kate Drax <katie.drax@bristol.ac.uk>
2020-04-30 15:04:35 +0100

    add more checks and comments to functions

--- rscripts/functions/clean-string-fun.R
+++ rscripts/functions/clean-string-fun.R
@@ -1,5 +1,7 @@
 # function to clean string 
 clean_string <- function(string){
+  if(is.character(string) == F) stop("string not character")
+  if(identical(string, character(0)) == T) stop("string empty")
   # string: a vector of strings
   # remove any non-english character, numbers, spaces or punctuation and lower
   string <- gsub("[^\u0001-\u007F]+","", string)
@@ -7,4 +9,5 @@ clean_string <- function(string){
   string <- gsub("[[:punct:]]", "", string)
   string <- tolower(string)
   string <- gsub("[[:space:]]", "", string)
+  return(string)
 }
--- rscripts/functions/find-conflicts-fun.R
+++ rscripts/functions/find-conflicts-fun.R
@@ -2,9 +2,12 @@
 find_cons <- function(df,a,b){
   if(is.null(a)) stop("a doesn't exist")
   if(is.null(b)) stop("b doesn't exist")
-  # a & b = two columns to be compared
-  # df = dataframe columns are in
-  # create df of katie's and Mark's columns that don't match and include NAs in matching
+  # a  = variable to be compared from reviewer1
+  # b = variable to be compared from reviewer2
+  # a & b should be identical before the suffixes
+  # df = dataframe columns are in >
+  # df 
+  # create df of reviewer1 and reviewer2's columns that don't match and include NAs in matching
   x <- df[which((!is.na(a) & !is.na(b) & a==b | is.na(a) & is.na(b)) == F),]
   # if conflicts - save x and present warning
   if(nrow(x) >0){
