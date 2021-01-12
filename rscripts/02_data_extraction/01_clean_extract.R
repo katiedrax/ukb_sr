1e7c792ba55177208786ff6b60cf13c50b8cc946
Katie Drax <katiedrax@gmail.com>
2021-01-12 10:55:26 +0000

    rename 01 and change data variable name to df

--- rscripts/02_data_extraction/01_clean_extract.R
+++ rscripts/02_data_extraction/01_clean_extract.R
@@ -15,7 +15,7 @@ input <- "data/data_extraction_form.csv"
 
 # Import data
 # read in "" as NA to avoid recognising Not Applicable (NA) values as missing
-data <- read.csv(input, encoding = "UTF-8", stringsAsFactors = F, header = F, na.strings = "")
+df <- read.csv(input, encoding = "UTF-8", stringsAsFactors = F, header = F, na.strings = "")
 
 # vector of question text associated with cols automatically outputted by Qualtrics 
 
@@ -31,11 +31,11 @@ qual_vars <- c("StartDate", "EndDate", "Status", "Progress", "Duration (in secon
 ################
 
 # drop all qual_vars if row 1 contains them 
-if(identical(qual_vars, as.character(data[1, 1:10]))){
-  qual_vars_data <- data
-  data <- data[, -c(1:10)]
+if(identical(qual_vars, as.character(df[1, 1:10]))){
+  qual_vars_data <- df
+  df <- df[, -c(1:10)]
 } else {
-  stop("data doesn't contain qual_vars")
+  stop("df doesn't contain qual_vars")
 }
 
 ###############
@@ -43,13 +43,13 @@ if(identical(qual_vars, as.character(data[1, 1:10]))){
 ###############
 
 # question names were not set in qualtrics  >
-# so without qual_text, data col names should all contain question numbers in the format Q[1-2 digit number] >
+# so without qual_text, df col names should all contain question numbers in the format Q[1-2 digit number] >
 # warn user if this isn't true
 
-if(sum(grepl("Q[[:digit:]]{1,2}", data[1, ])) != ncol(data)){
+if(sum(grepl("Q[[:digit:]]{1,2}", df[1, ])) != ncol(df)){
   stop("row 1 contains more than question numbers")
 } else {
-  rownames(data)[1] <- "q_num"
+  rownames(df)[1] <- "q_num"
 }
 
 # row 2 should contain full question text >
@@ -57,18 +57,18 @@ if(sum(grepl("Q[[:digit:]]{1,2}", data[1, ])) != ncol(data)){
 # so values in row 2 (except those that were 'matrix' questions) should have this format >
 # warn user if row 2 contains very few values that start "^(\w+)\."
 
-if(sum(grepl("^\\w+\\.", data[2,])) < 10){
+if(sum(grepl("^\\w+\\.", df[2,])) < 10){
   stop("row 2 doesn't contain variable names")
 } else {
-  rownames(data)[2] <- "q_text"
+  rownames(df)[2] <- "q_text"
 }
 
 # row 3 should contain import ids
 
-if(sum(grepl("ImportId", data[3,])) != ncol(data)){
-  stop("check data row 2 contains import ids")
+if(sum(grepl("ImportId", df[3,])) != ncol(df)){
+  stop("check df row 2 contains import ids")
 } else {
-  rownames(data)[3] <- "import_id"
+  rownames(df)[3] <- "import_id"
 }
 
 ###############
@@ -78,18 +78,18 @@ if(sum(grepl("ImportId", data[3,])) != ncol(data)){
 # find topic cols, check they are empty and remove them if they are
 # create vector of topic columns
 
-topic_col <- grep("topic", data[2,], ignore.case = T)
+topic_col <- grep("topic", df[2,], ignore.case = T)
 
 # check all topic cols empty besides first three rows containing q_num, q_text and 
 
 for(i in topic_col){
-  if(sum(!is.na(data[, i])) != 3) stop(i, "has more than 3 values")
+  if(sum(!is.na(df[, i])) != 3) stop(i, "has more than 3 values")
 }
 
 
 # drop random topic cols if both empty besides first three rows
 
-data <- data[, -topic_col]
+df <- df[, -topic_col]
 
 
 ################
@@ -100,20 +100,20 @@ data <- data[, -topic_col]
 # I put the variable name for non-strobe items at the start of the question so "[variable].">
 # find strobe and non-strobe cols
 
-strb_cols <- grep("\\- [[:digit:]]{1,}.*\\.", data["q_text", ])
-var_dot <- grep("^\\w+\\.", data["q_text", ])
+strb_cols <- grep("\\- [[:digit:]]{1,}.*\\.", df["q_text", ])
+var_dot <- grep("^\\w+\\.", df["q_text", ])
 
-# extract strobe and non-strobe variables from q_text, do not simplify so list is same length as ncol(data) when unlist
+# extract strobe and non-strobe variables from q_text, do not simplify so list is same length as ncol(df) when unlist
 
-var <- str_extract_all(data["q_text", ], "^\\w+\\.|\\- [[:digit:]]{1,}.*\\.", simplify = F)
+var <- str_extract_all(df["q_text", ], "^\\w+\\.|\\- [[:digit:]]{1,}.*\\.", simplify = F)
 
 # unlist
 
-if(length(var) == ncol(data)){
+if(length(var) == ncol(df)){
   # if same length unlist var but replace all NULL characters with NA so will be kept 
   var <- unlist(lapply(var,function(x) if(identical(x,character(0))) NA else x))
 } else {
-  stop("var and data are different lengths")
+  stop("var and df are different lengths")
 }
 
 # remove everything before digit or after .
@@ -124,8 +124,8 @@ var <- gsub("\\..*|- ", "", var)
 
 if(all(grepl("^[[:digit:]]", var[strb_cols]) == F)) stop("not all strobe start with digit")
 
-if(identical(length(var), ncol(data))){
-  colnames(data) <- var
+if(identical(length(var), ncol(df))){
+  colnames(df) <- var
 } else {
   stop("wrong length")
 }
@@ -136,11 +136,11 @@ a <- 1:length(var)
 
 for(i in 1:length(var)){
   # extract and save q_text value if contains colname
-  a[i] <- grep(colnames(data)[i], data["q_text", i], value = T)
+  a[i] <- grep(colnames(df)[i], df["q_text", i], value = T)
 }
 
 # if all q_text values contain colnames then a should be identical to the q_text row, check this
-if(identical(a, as.character(data["q_text", ])) == F) stop("var wrong")
+if(identical(a, as.character(df["q_text", ])) == F) stop("var wrong")
 
 ##################
 # edit header ####
@@ -148,74 +148,74 @@ if(identical(a, as.character(data["q_text", ])) == F) stop("var wrong")
 
 # affix all strobe variables with "s" to show they are strobe
 
-colnames(data)[strb_cols] <- paste("s", colnames(data)[strb_cols], sep = "")
+colnames(df)[strb_cols] <- paste("s", colnames(df)[strb_cols], sep = "")
 
 # Qualtrics allows you to add text boxes to multiple-choice questions >
 # text boxes for STROBE items were for evidence >
 # it indicates this by appending "_TEXT" to the question number or "- Text" to the question text >
 # find cols of question numbers containing _TEXT so can append 
 
-text_col <- grep("_TEXT", data["q_num",])
+text_col <- grep("_TEXT", df["q_num",])
 
-# append text to colnames(data) elements
+# append text to colnames(df) elements
 
-colnames(data)[text_col] <- paste(colnames(data)[text_col], "_ev", sep = "")
+colnames(df)[text_col] <- paste(colnames(df)[text_col], "_ev", sep = "")
 
 # there should be two strobe_ev variables now >
 # "strobe_ev." q_text gives evidence for use of strobe, "strobe. ... - Text" q_text details other reporting guidelines
 
-if(sum(colnames(data) == "strobe_ev" ) == 2){
+if(sum(colnames(df) == "strobe_ev" ) == 2){
   # if are dups get q_text col containing "strobe. ... - Text"
-  other_col <- grep("strobe\\..*- Text", data["q_text",])
+  other_col <- grep("strobe\\..*- Text", df["q_text",])
   if(length(other_col) != 1) stop("wrong cols") 
-  colnames(data)[other_col] <- "other_guidelines"
+  colnames(df)[other_col] <- "other_guidelines"
 } else {
   stop("not dups")
 }
 
 #################
-# make data dict and remove qualitrics rows ####
+# make df dict and remove qualitrics rows ####
 ######################
 
-# make data dictionary so list variables with original question text and the numbers qualtrics assigned them
-dict <- data.frame(variable = colnames(data), question = as.character(data["q_text", ]),
-                   qual_var = as.character(data["q_num", ]), stringsAsFactors = F)
+# make df dictionary so list variables with original question text and the numbers qualtrics assigned them
+dict <- df.frame(variable = colnames(df), question = as.character(df["q_text", ]),
+                   qual_var = as.character(df["q_num", ]), stringsAsFactors = F)
 
 # drop qualtrics rows
 
-data <- data[!rownames(data) %in% c("q_num", "q_text", "import_id"), ]
+df <- df[!rownames(df) %in% c("q_num", "q_text", "import_id"), ]
 
 # create cols for resolving conflicts
 
 
-x <- paste0(colnames(data)[grepl("_ev$", colnames(data)) == F], "_correct")
-correct_cols <- data.frame(matrix(ncol = length(x), nrow = nrow(data)))
+x <- paste0(colnames(df)[grepl("_ev$", colnames(df)) == F], "_correct")
+correct_cols <- df.frame(matrix(ncol = length(x), nrow = nrow(df)))
 colnames(correct_cols) <- x
 
 #add in correct_cols
 
-data <- cbind(data, correct_cols)
+df <- cbind(df, correct_cols)
 
 # order columns using mixedorder so strobe items keep numerical order
 
-data <- data[ , gtools::mixedorder(colnames(data))]
+df <- df[ , gtools::mixedorder(colnames(df))]
 
 
 #######
 # export ####
 #########
 
-# export data if passes checks
+# export df if passes checks
 
-if(sum(!is.na(colnames(data))) == ncol(data)){
+if(sum(!is.na(colnames(df))) == ncol(df)){
   # check Not applicable values are characters, check this in 7_iii strobe item which should contain lots of Not APplicables
-  if("NA" %in% data$s7_iii == F) stop("not applicables aren't characters")
+  if("NA" %in% df$s7_iii == F) stop("not applicables aren't characters")
   # check no colnames start with number cause R doesn't like it
-  if(sum(grepl("^[[:digit:]]", colnames(data))) != 0) stop("some colnames(data)s start with 0")
+  if(sum(grepl("^[[:digit:]]", colnames(df))) != 0) stop("some colnames(df)s start with 0")
   # check no duplicates
-  if(sum(duplicated(colnames(data))) != 0) stop("colnames(data) contains ", sum(duplicated(colnames(data))), " duplicates")
+  if(sum(duplicated(colnames(df))) != 0) stop("colnames(df) contains ", sum(duplicated(colnames(df))), " duplicates")
   # export NAs as blanks so don't confuse Not Applicable (NA) response
-  write.csv(data, "outputs/clean_extraction_form.csv", row.names = F, fileEncoding = "UTF-8", na = "")
+  write.csv(df, "outputs/clean_extraction_form.csv", row.names = F, fileEncoding = "UTF-8", na = "")
 } else {
   stop("wrong length")
 }
