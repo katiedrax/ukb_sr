5a830f4e6caabff0777d7ce26a69baf9337a36d2
katiedrax <katiedrax@gmail.com>
2021-01-26 12:29:00 +0000

    forgot to add new function in last commit, add now

--- rscripts/02_data_extraction/02_find_extract_conflicts.R
+++ rscripts/02_data_extraction/02_find_extract_conflicts.R
@@ -11,9 +11,21 @@ library(tidyr)
 # functions ####
 ##############
 
-# get clean string function
+# create clean string function
 
-source("rscripts/functions/clean-string-fun.R")
+# function to clean string 
+clean_string <- function(string){
+  if(is.character(string) == F) stop("string not character")
+  if(identical(string, character(0)) == T) stop("string empty")
+  # string: a vector of strings
+  # remove any non-english character, numbers, spaces or punctuation and lower
+  string <- gsub("[^\u0001-\u007F]+|<U\\+\\w+>","", string)
+  string <- gsub("[0-9]", "", string)
+  string <- gsub("[[:punct:]]", "", string)
+  string <- tolower(string)
+  string <- gsub("[[:space:]]", "", string)
+  return(string)
+}
 
 ##########
 # import####
